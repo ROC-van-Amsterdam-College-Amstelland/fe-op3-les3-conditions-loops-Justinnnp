2afeba0a1e6276af30b1eb8a659a6fea3ae77995
Justin Plein <justin.plein1@student.rocva.nl>
2019-11-25 15:06:57 +0100

    Opdracht 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,13 @@
 //javascript code
+function checken() {
+    var saldo = document.getElementById("saldo").value;
+
+    if(saldo == 500) {
+        document.getElementById("tekst").innerHTML = "Er is 500 euro op je rekening gestort !";
+    } 
+    else if(saldo >= 500) {
+        document.getElementById("tekst").innerHTML = "Er is 500 euro op je rekening en een bonus !"
+    } else {
+        document.getElementById("tekst").innerHTML = "Sorry, er is geen geld op je tekening gestort !"
+    }
+}
