4984010a1c7324c5f83d88a9587b56124746a856
Justin Plein <justin.plein1@student.rocva.nl>
2019-11-25 14:51:06 +0100

    Opdracht 1

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,10 @@
 //javascript code
+function checken() {
+    var saldo = document.getElementById("saldo").value;
+
+    if(saldo<25) {
+        document.getElementById("tekst").innerHTML = "Sorry, u hebt te weinig saldo !";
+    } else {
+        document.getElementById("tekst").innerHTML = "uw saldo is prima !"
+    }
+}
