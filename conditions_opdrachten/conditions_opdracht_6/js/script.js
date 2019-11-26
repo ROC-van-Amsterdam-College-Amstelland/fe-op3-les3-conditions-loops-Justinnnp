0f3fdef39a77e814b9338f23de3062882069b467
Justin Plein <justin.plein1@student.rocva.nl>
2019-11-26 13:17:19 +0100

    opdracht 6

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+calculate = function () {
+    var getal1 = document.getElementById('a1').value;
+    var getal2 = document.getElementById('a2').value;
+    document.getElementById('a3').value = parseInt(getal1) * parseInt(getal2);
+
+}
+
+calculate2 = function () {
+    var getal1 = document.getElementById('b1').value;
+    var getal2 = document.getElementById('b2').value;
+    document.getElementById('b3').value = parseInt(getal1) / parseInt(getal2);
+
+}
