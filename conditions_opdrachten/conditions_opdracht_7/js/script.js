770f191c2de8abdbd2ad72c0626d86a6b0346163
Justin Plein <justin.plein1@student.rocva.nl>
2019-11-26 13:35:33 +0100

    opdracht 7 maar niet af.

--- conditions_opdrachten/conditions_opdracht_7/js/script.js
+++ conditions_opdrachten/conditions_opdracht_7/js/script.js
@@ -1,1 +1,9 @@
 //javascript code
+
+// deze functie veranderd de kleur doormiddel van: een functie, if, else if en else.
+function changeColor() {
+
+    if(nummer == 10) {
+        document.body.style.backgroundColor = "green";
+    }
+}
