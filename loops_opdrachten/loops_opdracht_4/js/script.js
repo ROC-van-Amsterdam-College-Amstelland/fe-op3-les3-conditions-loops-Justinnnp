ab099449dda33c634476350a09fa00450aa4d484
Justin Plein <justin.plein1@student.rocva.nl>
2019-12-02 15:17:45 +0100

    opracht 4

--- loops_opdrachten/loops_opdracht_4/js/script.js
+++ loops_opdrachten/loops_opdracht_4/js/script.js
@@ -1,1 +1,12 @@
 //javascript code
+var tekst;
+for (var i = 0; i <= 1000; i += 10) {
+
+    tekst += i + "<br>";
+
+    console.log(i);
+    
+
+}
+
+document.getElementById("uitkomst").innerHTML = tekst;
