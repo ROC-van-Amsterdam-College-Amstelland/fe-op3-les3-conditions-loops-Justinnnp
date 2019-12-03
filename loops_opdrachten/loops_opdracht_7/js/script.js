947ed26a326cabc6a177fa0bcdaafcf445c32f93
Justin Plein <justin.plein1@student.rocva.nl>
2019-12-03 12:41:35 +0100

    òpdracht 7 loops

--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+var tekst = "";
+for (var i = 1; i <= 100; i += 1) {
+
+    tekst += i + "<br>";
+
+    if (i % 3) {
+        tekst += "VET" + "<br>";
+    } else if (i % 5) {
+        tekst += "COOL"  + "<br>";
+    }
+
+}
+
+document.getElementById("uitkomst").innerHTML = tekst;
