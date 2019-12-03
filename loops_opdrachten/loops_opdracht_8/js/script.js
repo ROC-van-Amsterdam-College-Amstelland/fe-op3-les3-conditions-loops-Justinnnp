fa3f5031e44a7801be6ada945026e2428b9647ba
Justin Plein <justin.plein1@student.rocva.nl>
2019-12-03 12:51:56 +0100

    opdracht 8 loops kleine aanpassing.

--- loops_opdrachten/loops_opdracht_8/js/script.js
+++ loops_opdrachten/loops_opdracht_8/js/script.js
@@ -5,7 +5,7 @@ for (var i = 0; i <= 50; i += 1) {
     tekst += i + "<br>";
 
     if(i == 25) {
-        parseInt(alert("hetettstste"));
+        parseInt(alert("Het getal 25 is net geen bullseye !"));
     }
     console.log(i);
     
