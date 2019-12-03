6044626dc461bde216064d982680492075862d15
Justin Plein <justin.plein1@student.rocva.nl>
2019-12-03 12:50:14 +0100

    opdracht 8 loops

--- loops_opdrachten/loops_opdracht_8/js/script.js
+++ loops_opdrachten/loops_opdracht_8/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+var tekst ="";
+for (var i = 0; i <= 50; i += 1) {
+
+    tekst += i + "<br>";
+
+    if(i == 25) {
+        parseInt(alert("hetettstste"));
+    }
+    console.log(i);
+    
+
+}
+
+document.getElementById("uitkomst").innerHTML = tekst;
